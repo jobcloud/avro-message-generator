71e69cd526bae5c4a65f080984b441bb3c7efba7
shoky90 <shoky90@yahoo.com>
2020-10-20 16:21:29 +0200

    PayloadGenerator

--- src/Payload/Generator.php
+++ src/Payload/Generator.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Jobcloud\Avro\Message\Generator\Payload;
 
+use DateTime;
 use Faker\Generator as Faker;
+use InvalidArgumentException;
 use Jobcloud\Avro\Message\Generator\Exception\UnsupportedAvroSchemaTypeException;
 use Jobcloud\Avro\Message\Generator\Schema\AvroSchemaTypes;
 
@@ -61,10 +63,11 @@ class Generator implements GeneratorInterface
     /**
      * @param array<string, mixed> $decodedSchema
      * @param mixed $predefinedPayload
+     * @param bool $isReadableSchema
      * @return mixed
      * @throws UnsupportedAvroSchemaTypeException
      */
-    private function getPayload(array $decodedSchema, $predefinedPayload = null)
+    private function getPayload(array $decodedSchema, $predefinedPayload = null, bool $isReadableSchema = false)
     {
         switch ($decodedSchema['type']) {
             case AvroSchemaTypes::NULL_TYPE:
@@ -74,24 +77,31 @@ class Generator implements GeneratorInterface
                 $payload = rand(0, 1) == 1;
                 break;
             case AvroSchemaTypes::INT_TYPE:
-                $payload = rand(1, 10000);
-                break;
             case AvroSchemaTypes::LONG_TYPE:
-                // nas
+                $payload = rand(1, 100000);
                 break;
             case AvroSchemaTypes::FLOAT_TYPE:
-
-                break;
             case AvroSchemaTypes::DOUBLE_TYPE:
-
+                $payload = (float) rand() / (float) getrandmax();
                 break;
             case AvroSchemaTypes::BYTES_TYPE:
-
+                $payload = pack('C', rand(1, 100));
                 break;
             case AvroSchemaTypes::STRING_TYPE:
+                $payload = $this->faker->word;
+
+                if ($isReadableSchema && isset($decodedSchema['name'])) {
+                    $payload = $this->getReadableStringByFieldName($decodedSchema);
+                }
 
                 break;
             case AvroSchemaTypes::RECORD_TYPE:
+
+                $payload = [];
+
+                foreach ($decodedSchema['fields'] as $field) {
+                    $payload[$field['name']] = $this->getPayload($field);
+                }
 
                 break;
             case AvroSchemaTypes::ENUM_TYPE:
@@ -103,18 +113,160 @@ class Generator implements GeneratorInterface
             case AvroSchemaTypes::MAP_TYPE:
 
                 break;
-            case AvroSchemaTypes::FIXED_TYPE:// nas check this and enum
+            case AvroSchemaTypes::FIXED_TYPE:// nas check this
 
                 break;
             default:
-                throw new UnsupportedAvroSchemaTypeException(sprintf(
-                    'Schema type "%s" is not supported by Avro.',
-                    $decodedSchema['type']
-                ));
+                if (is_array($decodedSchema['type'])) {
+                    if ($decodedSchema['type'] === array_values($decodedSchema['type'])) {
+                        // UNION TYPE
+                        $payload = $this->extractPayloadFromUnionField($decodedSchema);
+                    }
+
+                    if (isset($decodedSchema['type']['type'])) {
+                        // NESTED SCHEMA
+                        $payload = $this->getPayload($decodedSchema['type'], null, true);
+                    }
+                }
+
+                if (!isset($payload)) {
+                    throw new UnsupportedAvroSchemaTypeException(sprintf(
+                        'Schema type "%s" is not supported by Avro.',
+                        $decodedSchema['type']
+                    ));
+                }
         }
 
         return $payload;
     }
+
+    /**
+     * @param array $decodedSchema
+     * @return string
+     */
+    private function getReadableStringByFieldName(array $decodedSchema): string
+    {
+        $fieldName = $decodedSchema['name'];
+
+        try {
+            return (string) $this->faker->{$fieldName};
+        } catch (InvalidArgumentException $e) {
+            if ($fieldName === 'id' || mb_substr($fieldName, -2) === 'Id') {
+                return $this->faker->uuid;
+            }
+
+            if (mb_substr($fieldName, -2) === 'At') {
+                return (new DateTime())->format(DateTime::ATOM);
+            }
+
+            $predefinedOption = $this->extractPredefinedOptionFromDocField($decodedSchema);
+
+            if ('' !== $predefinedOption) {
+                return $predefinedOption;
+            }
+
+            if ($fieldName === 'language') {
+                return $this->faker->languageCode;
+            }
+
+            if ($fieldName === 'postalCode') {
+                return $this->faker->postcode;
+            }
+
+            if ($fieldName === 'street') {
+                return $this->faker->streetName;
+            }
+        }
+
+        return $this->faker->word;
+    }
+
+    /**
+     * @param array $decodedSchema
+     * @return string
+     */
+    private function extractPredefinedOptionFromDocField(array $decodedSchema): string
+    {
+        if (!isset($decodedSchema['doc'])) {
+            return '';
+        }
+
+        $doc = $decodedSchema['doc'];
+
+        if (false !== mb_strpos($doc, "ISO-8601")) {
+            return (new DateTime())->format(DateTime::ATOM);
+        }
+
+        $openBracketPosition = mb_strpos($doc, '[');
+
+        $closeBracketPosition = mb_strpos($doc, ']');
+
+        if (false === $openBracketPosition || false === $closeBracketPosition) {
+            return '';
+        }
+
+        if ($openBracketPosition > $closeBracketPosition) {
+            return '';
+        }
+
+        $options = array_map(
+            fn($item) => trim($item, "[ ']"),
+            explode(",", mb_substr($doc, $openBracketPosition + 1, $closeBracketPosition - $openBracketPosition - 1))
+        );
+
+        shuffle($options);
+
+        return $options[0];
+    }
+
+    /**
+     * @param array $decodedSchema
+     * @return mixed
+     * @throws UnsupportedAvroSchemaTypeException
+     */
+    private function extractPayloadFromUnionField(array $decodedSchema)
+    {
+        $types = $decodedSchema['type'];
+
+        $extractedPayloads = [];
+
+        foreach ($types as $type) {
+            $decodedSchema['type'] = $type;
+
+            $extractedPayloads[] = $this->getPayload($decodedSchema, null, true);
+        }
+
+        shuffle($extractedPayloads);
+
+        return $extractedPayloads[0];
+    }
+//        if ($fieldName === 'url') {
+//            return $this->faker->url;
+//        }
+//
+//        if ($fieldName === 'text') {
+//            return $this->faker->text;
+//        }
+//
+//        if ($fieldName === 'email') {
+//            return $this->faker->email;
+//        }
+//
+//        if ($fieldName === 'name') {
+//            return $this->faker->name;
+//        }
+//
+//        if ($fieldName === 'title') {
+//            return $this->faker->title;
+//        }
+//
+//        if ($fieldName === 'firstName') {
+//            return $this->faker->firstName;
+//        }
+//
+//        if ($fieldName === 'lastName') {
+//            return $this->faker->lastName;
+//        }
 //
 //    /**
 //     * @param array $payload
